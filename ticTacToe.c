31f498a7c234bb4f10eaed323e429fd2c8493b7c
Joe O'Regan <k00203642@student.lit.ie>
2017-12-23 23:23:59 +0000

    26/10/2015 c Check loop

--- ticTacToe.c
+++ ticTacToe.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
-	// Global Variables
+
+
+    //char grid[3][3]={'1','2','3','4','5','6','7','8','9'};		// function 1
     int grid[3][3]={1,2,3,4,5,6,7,8,9};								// function 1			original grid
     int input; 														// function 2, 3, 4
     int player;														// function 2, 3, 4
@@ -18,8 +20,8 @@ main()
 	printf("\nPlayer 1 = X\n");
 	printf("Player 2 = O\n\n");
 	
-    printGrid();					// print original integer grid
-	roundCount();					// until gameOver changes to 1
+    printGrid();				// print original integer grid
+	playGame();					// until gameOver changes to 1
 	
 	//printf("\n\nContinue? Press x to exit\n");
 	//scanf(" %c",exit);
@@ -47,55 +49,55 @@ printGrid()
        printf("%d ",grid[x][y]);
       	   
 	   if (y<2)printf("| ");
-      }													// y
+      } // y
       if(x<2)printf("\n__________\n");
-     }													// x
+     } // x
 }
 
 /*****************************************************/
-//				Function 2: Round Counter
+//				Function 2: play game
 /*****************************************************/
 
-roundCount()
+playGame()
 {
 	int round=0;
-	while(round<6)
-	{	
-	 printf("\n\nROUND %d:\n",round+1);					// while(gameOver!=1) *working without ***CHECK
-	 	
-	 playerTurn();
-     ++round;											// increment round
-	}
-}														// funct
-
-/*****************************************************/
-//				Function 3: Player Turn
-/*****************************************************/
-
-playerTurn()
-{
-	int i;
-	while((gameOver!=1)&&(player<3))					// player 2 goes to round 4 *fixed
+	
+	while(gameOver!=1)						// ERROR: goes to round 4 for player 2
+	//while(round<5)
+	//do 									// ERROR: goes to round 4 for player 2
 	{
-	 for(i=1;i<=2;i++)
-	 { 	 
-	  winnerTest();	
-	  if(gameOver!=1)  									// needs to be here	or player turn keeps going after game over			
+	 printf("\n\nROUND %d:\n",round+1);
+	 player=1;
+	 	
+	 while(player<3)
+	 //do
+	 {
+	  winnerTest(); 						// ERROR: goes to already x or o
+	  
+	  if((gameOver!=1) && (player < 3))
 	  {
-	  printf("\nPlayer %d: please select postion 1-9: ",player=i);
+	  //winnerTest(); 						// ERROR: goes to player 4 at end
+	  //if(gameOver!=1){
+	  printf("\nPlayer %d: please select postion 1-9: ",player);
 	  scanf(" %d",&input);
-	 
-	  checkInput();										// Increment Player **has to go after checkInput() **for loop not working
-	  }
+	  //}
+	  
+	  checkInput1to9();
+	  ++player;
+	  }// 									// replace previous bracket -- still goes to round 4 before exit
 	 }
-	}
+     //while(player<3);		// player go
+     
+     ++round;
+	}	// round
+	//while(gameOver!=1);
 }		// funct
 
 /*****************************************************/
-//		Function 4: Check if user input is 1 to 9
+//		Function 3: Check if user input is 1 to 9
 /*****************************************************/
 
-checkInput() 											// check input is 1 to 9
+checkInput1to9()
 {
 	int x,y;
 	
@@ -103,27 +105,27 @@ checkInput() 											// check input is 1 to 9
 	{
 	 for(y=0;y<3;++y)
 	 {
-	  if (input==grid[x][y])							// Check for valid input
+	  if (input==grid[x][y])
 	  {
-	  	checkInputXorO();								// test for already x or o
-		check1to9=1;									
+	  	checkInputXorO();		// test for already x or o
+		check1to9=1;
 	  }
-	 }													// y
-	}													// x
+	 }	//y
+	}	//x
 	
 	if((check1to9==1)&&(checkXorO==1))
 	{
-	 editGrid();										// print Output array
+	 editGrid();
 	}
 	else 
 	{
 	 printf("Between 1 to 9!!! Try Again");
-	  --player;					 						// RESET THE PLAYERS MOVE!!!!!!
+	  --player;					 // RESET THE PLAYERS MOVE!!!!!!
 	}
 }
 
 /*****************************************************/
-//		Function 5: Check if X or O already
+//		Function 4: Check if X or O already
 /*****************************************************/
 
 checkInputXorO()
@@ -141,21 +143,21 @@ checkInputXorO()
 		 printf("\nAlready X - Please Try Again\n");
 		 --player;
 		}
-		else checkXorO=1;								// OK to proceed
+		else checkXorO=1;								// ok to proceed
 		
 	  	if(arrOutput[x][y]=='O')
 		{
 		 printf("\nAlready O - Please Try Again\n");
-		 --player;										// Reset Player Go
+		 --player;
 		}
-		else checkXorO=1;								// OK to proceed
+		else checkXorO=1;								// ok to proceed
 	  }
-	 }													// y
-	}													// x
+	 }	//y
+	}	//x
 }
 
 /*****************************************************/
-//			Function 6: Output the Grid
+//			Function 5: Output the Grid
 /*****************************************************/
 
 editGrid()
@@ -166,62 +168,72 @@ editGrid()
 	 {
 	  for(j=0;j<3;++j)
 	  {
+	  	//printf("%c",input);
+	  	
 	   if (input==grid[i][j])
 		{
-		 if(player==1)arrOutput[i][j]='X';				// player 1 = X
-		 if(player==2)arrOutput[i][j]='O'; 				// player 2 = O
+		 if(player==1)arrOutput[i][j]='X'; // player 1 = X
+		 if(player==2)arrOutput[i][j]='O'; // player 2 = O
 	    }
 	    printf("%c ",arrOutput[i][j]);
         if (j<2) printf("| ");
-	   } 												// j
+	   } // j
 	  if(i<2)printf("\n__________\n");
 	  else printf("\n");
-	 }													// i
-}														// funct
+	 }	// i
+}		// funct
 
 /*****************************************************/
-//				Function 7: Check Winner
+//				Function 6: Check Winner
 /*****************************************************/
 
 winnerTest()
 {
-	int i,j,winner=0;
+	int i,j,winner;
 	
+	//check rows
 	for(i=0;i<3;++i)
 	{
 	 for(j=0;j<3;++j)
 	 {
 	  // Player 1
 	  // rows
-	  if((arrOutput[0][0]=='X') && (arrOutput[0][1]=='X') && (arrOutput[0][2]=='X')) winner=1;	// 123
-	  if((arrOutput[1][0]=='X') && (arrOutput[1][1]=='X') && (arrOutput[1][2]=='X')) winner=1;	// 456
-	  if((arrOutput[2][0]=='X') && (arrOutput[2][1]=='X') && (arrOutput[2][2]=='X')) winner=1;	// 789
+	  if((arrOutput[0][0]=='X') && (arrOutput[0][1]=='X') && (arrOutput[0][2]=='X')) winner=1; // 123
+	  if((arrOutput[1][0]=='X') && (arrOutput[1][1]=='X') && (arrOutput[1][2]=='X')) winner=1; // 456
+	  if((arrOutput[2][0]=='X') && (arrOutput[2][1]=='X') && (arrOutput[2][2]=='X')) winner=1; // 789
 	  // columns
-	  if((arrOutput[0][0]=='X') && (arrOutput[1][0]=='X') && (arrOutput[2][0]=='X')) winner=1;	// 147
-	  if((arrOutput[0][1]=='X') && (arrOutput[1][1]=='X') && (arrOutput[2][1]=='X')) winner=1;	// 258
-	  if((arrOutput[0][2]=='X') && (arrOutput[1][2]=='X') && (arrOutput[2][2]=='X')) winner=1;	// 369
+	  if((arrOutput[0][0]=='X') && (arrOutput[1][0]=='X') && (arrOutput[2][0]=='X')) winner=1; // 147
+	  if((arrOutput[0][1]=='X') && (arrOutput[1][1]=='X') && (arrOutput[2][1]=='X')) winner=1; // 258
+	  if((arrOutput[0][2]=='X') && (arrOutput[1][2]=='X') && (arrOutput[2][2]=='X')) winner=1; // 369
 	  // diagonals
-	  if((arrOutput[0][0]=='X') && (arrOutput[1][1]=='X') && (arrOutput[2][2]=='X')) winner=1;	// 159
-	  if((arrOutput[0][2]=='X') && (arrOutput[1][1]=='X') && (arrOutput[2][0]=='X')) winner=1;	// 357
+	  if((arrOutput[0][0]=='X') && (arrOutput[1][1]=='X') && (arrOutput[2][2]=='X')) winner=1; // 159
+	  if((arrOutput[0][2]=='X') && (arrOutput[1][1]=='X') && (arrOutput[2][0]=='X')) winner=1; // 357
 	  
 	  // Player 2
 	  // rows
-	  if((arrOutput[0][0]=='O') && (arrOutput[0][1]=='O') && (arrOutput[0][2]=='O')) winner=2;	// 123
-	  if((arrOutput[1][0]=='O') && (arrOutput[1][1]=='O') && (arrOutput[1][2]=='O')) winner=2;	// 456
-	  if((arrOutput[2][0]=='O') && (arrOutput[2][1]=='O') && (arrOutput[2][2]=='O')) winner=2;	// 789
+	  if((arrOutput[0][0]=='O') && (arrOutput[0][1]=='O') && (arrOutput[0][2]=='O')) winner=2; // 123
+	  if((arrOutput[1][0]=='O') && (arrOutput[1][1]=='O') && (arrOutput[1][2]=='O')) winner=2; // 456
+	  if((arrOutput[2][0]=='O') && (arrOutput[2][1]=='O') && (arrOutput[2][2]=='O')) winner=2; // 789
 	  // columns
-	  if((arrOutput[0][0]=='O') && (arrOutput[1][0]=='O') && (arrOutput[2][0]=='O')) winner=2;	// 147
-	  if((arrOutput[0][1]=='O') && (arrOutput[1][1]=='O') && (arrOutput[2][1]=='O')) winner=2;	// 258
-	  if((arrOutput[0][2]=='O') && (arrOutput[1][2]=='O') && (arrOutput[2][2]=='O')) winner=2;	// 369
+	  if((arrOutput[0][0]=='O') && (arrOutput[1][0]=='O') && (arrOutput[2][0]=='O')) winner=2; // 147
+	  if((arrOutput[0][1]=='O') && (arrOutput[1][1]=='O') && (arrOutput[2][1]=='O')) winner=2; // 258
+	  if((arrOutput[0][2]=='O') && (arrOutput[1][2]=='O') && (arrOutput[2][2]=='O')) winner=2; // 369
 	  // diagonals
-	  if((arrOutput[0][0]=='O') && (arrOutput[1][1]=='O') && (arrOutput[2][2]=='O')) winner=2;	// 159
-	  if((arrOutput[0][2]=='O') && (arrOutput[1][1]=='O') && (arrOutput[2][0]=='O')) winner=2;	// 357
+	  if((arrOutput[0][0]=='O') && (arrOutput[1][1]=='O') && (arrOutput[2][2]=='O')) winner=2; // 159
+	  if((arrOutput[0][2]=='O') && (arrOutput[1][1]=='O') && (arrOutput[2][0]=='O')) winner=2; // 357
 	 }	// j
 	}	// i
 	
-	if((gameOver!=1)&&((winner==1) || (winner==2)))
+	if(winner==1)
 	{
-	printf("\n\nGame Over\nPlayer %d Is The Winner", winner);									// terminate rounds loop
-	gameOver=1;
+	 printf("\n\nGAME OVER\nPlayer 1 Is The Winner");
+	 player+=2;		// terminate player 1 or 2 turn loop
+	 gameOver=1;	// terminate rounds loop
+	}	
+	if(winner==2)
+	{
+	 printf("\n\nGAME OVER\nPlayer 2 Is The Winner");
+	 player+=2;
+	 gameOver=1;
 	}	
 }
