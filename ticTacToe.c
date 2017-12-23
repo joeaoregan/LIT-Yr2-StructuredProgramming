e715ac465604dd1fa16b848af973d629e1de3e41
Joe O'Regan <k00203642@student.lit.ie>
2017-12-23 22:35:07 +0000

    24/10/2015 b

--- ticTacToe.c
+++ ticTacToe.c
@@ -8,73 +8,66 @@ main()
 {
    
       
-    int i=0,j=0,k=0,l=0;
+    int i=0,j=0;
+	//int k=0,l=0;
     
     int user;
     char grid[3][3]={'1','2','3','4','5','6','7','8','9'};
     
     printGrid(i,j);
 
+	gameRound();
 
-for(l=1;l<=5;++l) // rounds loop
-{
-	printf("\n\nROUND %d:\n",l);
-	
-	for(k=1;k<=2;++k) // Player goes
-  	{
-     
-     printf("\nPlayer %d: please select postion 1-9: ",k);
-     scanf(" %c",&user);
-    
-     for(i=0;i<3;++i)
-     {
-      for(j=0;j<3;++j)
-      {
-       if (user==grid[i][j])
-       {
-        if(k==1)grid[i][j]='O'; // player 1 = O
-        if(k==2)grid[i][j]='X'; // player 2 = X
-       // printGrid(i,j,user); //test NO
-       }
-      printf("%c ",grid[i][j]);
-      if (j<2)printf("| ");
-      
-      } // ***end of j for
-      if(i<2)printf("\n__________\n");
-      else printf("\n");
-      //printGrid(i,j); //test NO - twice repeat
-     } // ***end of i for
-     //printGrid(i,j); not working
-     
-    } // ***end of k (player goes for loop)
-} // end of l loop
+
     getch();
     return(0);
  } // end of main
 
 
-
+// function 1: Print Grid
 printGrid(int x, int y)
 {  
- char grid[3][3]={'1','2','3','4','5','6','7','8','9'};
+	char grid[3][3]={'1','2','3','4','5','6','7','8','9'};
 
-    printf("Select Position On Grid:\n");
+	printf("Select Position On Grid:\n");
  
     for(x=0;x<3;++x)
+    {
+     for(y=0;y<3;++y)
      {
-	  //if(x<3)printf("_ _ _"); // test grid
-      for(y=0;y<3;++y)
-      {
-       printf("%c ",grid[x][y]);
-      // if (user2==grid[x][y])
-      // {
-       // if(k==1)grid[x][y]='O'; // player 1 = O
-       // if(k==2)grid[x][y]='X'; // player 2 = X
-        //printGrid(i,j); //test NO
-      // }
-	   
-	   if (y<2)printf("| ");
-      } 
-      if(x<2)printf("\n__________\n");
-     } // end of x
+      printf("%c ",grid[x][y]);
+	  if (y<2)printf("| ");
+     }
+     if(x<2)printf("\n__________\n");
+    }
+     
+} // end function
+
+
+// Function 2: Game Rounds
+gameRound()
+{
+	int r; // round counter
+	for(r=1;r<=5;++r)
+	{
+	printf("\n\nROUND %d:\n",r);
+	playerGoes();
+	} // end of r
 }
+
+// Function 3: Player Goes
+playerGoes()
+{
+	int x=0,y=0,z=0;
+	char user;
+	
+	for(z=1;z<=2;++z) // Player goes
+  	{
+     
+     printf("\nPlayer %d: please select postion 1-9: ",z);
+     scanf(" %c",&user);
+     
+	}
+    
+} // end function
+
