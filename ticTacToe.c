e93ecec0d01736ffed21b969594172a327fb1d8e
Joe O'Regan <k00203642@student.lit.ie>
2017-12-23 23:05:23 +0000

    25/10/2015 j Check bit by bit

--- ticTacToe.c
+++ ticTacToe.c
@@ -1,18 +1,19 @@
-#include<stdio.h>
-// global variables
-	char user;
-    char grid[3][3]={'1','2','3','4','5','6','7','8','9'};			// original grid
-	//char outputGrid[3][3]={'1','2','3','4','5','6','7','8','9'};	// edited grid
-	int player;
-	
-//void testUserInput(int k2,char testInput);
-    
+#include <stdio.h>
+
+
+    //char grid[3][3]={'1','2','3','4','5','6','7','8','9'};	// function 1
+    int grid[3][3]={1,2,3,4,5,6,7,8,9};						// function 1
+    int user; 												// function 2, 3
+    int player;												// function 2, 3
+
 main()
 {
     printGrid();
-	//playGame();
-
-    getch();
+	playGame();  // Goes through 5 rounds
+	
+	printf("\nUser: %c",user);
+	
+	getch();
     return(0);
 } // main
 
@@ -27,59 +28,41 @@ printGrid()
      {
       for(y=0;y<3;++y)
       {
-       printf("%c ",grid[x][y]);
+       printf("%d ",grid[x][y]); //changed to int
       	   
 	   if (y<2)printf("| ");
       } // y
       if(x<2)printf("\n__________\n");
      } // x
 }
-/*
+
 // function 2: play game
 playGame()
 {
-	int player=0,round;
+	int round;
+	player=1;
 	
 	for(round=0;round<=5;++round) // rounds loop
 	{
-	 printf("\n\nROUND %d:\n",round);
-	 for(player=1;player<=2;++player) // Player goes
+	 printf("\n\nROUND %d:\n",round+1);
+	 
+	 //for(player=1;player<=2;++player) // Player goes
+	 do
 	 {
 	  printf("\nPlayer %d: please select postion 1-9: ",player);
-	  scanf(" %c",&user);
+	  scanf(" %d",&user);											// scan as int ***
 	  
-	 // checkInput(); // **************test Input Function
-	  
-     }	// k
-	}	// l
+	  checkInput();
+	  ++player;
+	 }	// player go
+     while(player<3);
+	}	// round
 }		// funct
 
-// Function 3: Edit Grid
-editGrid()
-{
-	int i,j;
-		  
-	 for(i=0;i<3;++i)
-	 {
-	  for(j=0;j<3;++j)
-	  {
-	   if (user==outputGrid[i][j])
-		{
-		 if(player==1)outputGrid[i][j]='O'; // player 1 = O
-		 if(player==2)outputGrid[i][j]='X'; // player 2 = X
-	    }
-	    printf("%c ",outputGrid[i][j]);
-        if (j<2) printf("| ");
-	   } // j
-	  if(i<2)printf("\n__________\n");
-	  else printf("\n");
-	 }	// i
-}		// funct
-
-// Function 4: Check User Input
+// Function 3: Check User Input
 checkInput()
 {
-	int x,y,temp;
+	int x,y;
 	int checkAvailable;
 	
 	for(x=0;x<3;++x)
@@ -93,14 +76,14 @@ checkInput()
 	 }	//y
 	}	//x
 	
-	if(temp==1) 
+	if(checkAvailable==1) 
 	{
 	 printf("Valid Move");
 	 //editGrid();
 	}
 	else 
 	{
-	 printf("invalid move");
+	 printf("Between 1 to 9!!! Try Again");
+	 --player;					 // RESET THE PLAYERS MOVE!!!!!!
 	}	
 }
-*/
