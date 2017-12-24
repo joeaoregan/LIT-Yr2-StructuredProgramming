eb2b91225bcb01e2f46f25a7023afbeebe84e005
Joe O'Regan <k00203642@student.lit.ie>
2017-12-24 05:08:40 +0000

    One function to draw the grid, and added reset function

--- ticTacToe.c
+++ ticTacToe.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
-int grid[3][3]={1,2,3,4,5,6,7,8,9};									// function 1		original grid
-int input, player;											// function 2, 3, 4
-char arrOutput[3][3]={'1','2','3','4','5','6','7','8','9'};						// function 4, 5 	edited grid
+int player;												// Which players turn
+int grid[3][3] = {1, 2, 3, 4, 5, 6, 7, 8, 9};								// original grid
+char arrOutput[3][3] = {'1','2','3','4','5','6','7','8','9'};						// edited grid
     
 int main() {
 	char exit = 'n';
@@ -10,50 +10,39 @@ int main() {
 	while (exit != 'x') {
 		printf("Tic Tac Toe\n\nPlayer 1 = X\nPlayer 2 = O\n\n");
 		
-	    	printGrid();										// print original integer grid
+		displayGrid(0);										// print original integer grid
 		playGame();										// until gameOver changes to 1
 		
 		printf("\n\nContinue? Press x to exit\n");
 		
-		//scanf("%c",exit);
-		exit = getc(stdin);
+		exit = gets(stdin);
+		//fgets(exit, 3, stdin);
+		reset();
 	} 
 	
     return(0);
-} // main
+} 
 
-// Function 1: print original grid
-printGrid() {  
-	int x, y;
-    printf("Select Position On Grid:\n");
- 
-	for(x = 0; x < 3; ++x)  {
-		for(y = 0; y < 3; ++y) {
-			printf("%d ",grid[x][y]);			
-			if (y<2)printf("| ");
-		} // y
-		
-		if( x < 2) printf("\n__________\n");
-	} // x
-}
-
-// Function 2: play game
+// Play the game
 void playGame() {
-	int round = 0, gameOver = 0;
+	int input, round = 1, gameOver = 0;
 	
-	while(gameOver!=1) {										// ERROR: goes to round 4 for player 2 - while(round<5)
-		printf("\n\nROUND %d:\n", round + 1);
+	while(gameOver != 1) {										// ERROR: goes to round 4 for player 2 - while(round<5)
+		printf("\n\nROUND %d:\n", round);
 		player = 1;										// Start with Player 1
 		 	
-		while(player < 3) {
-			gameOver = winnerTest(); 							// ERROR: goes to already x or o
+		while(player < 3) {									// Take turns for 2 players
+			gameOver = winnerTest(); 						
 			
 			if((gameOver != 1) && (player < 3)) {
-				printf("\nPlayer %d: please select postion 1-9: ",player);
-				scanf(" %d",&input);
-				
-				if (checkInput() == 1) {						// If the character entered is OK
-					editGrid();							// Display the grid
+				printf("\nPlayer %d: please select postion 1-9: ", player);
+				scanf(" %d", &input);							// Get input from player
+				//char* in;
+				//gets(in);
+				//input = atoi(gets(in));
+								
+				if (checkInput(input) == 1) {						// If the character entered is OK
+					displayGrid(input);						// Display the grid
 					player++;							// Next player turn
 				}
 			} 										// replace previous bracket -- still goes to round 4 before exit
@@ -63,13 +52,13 @@ void playGame() {
 	}	// round
 }
 
-// Function 3: Check if user input is 1 to 9
-int checkInput() {
-	int x,y, success = 0;
-	
+// Check if user input is 1 to 9
+int checkInput(int playerMove) {
+	int x, y, success = 0;
+		
 	for(x = 0; x < 3; ++x) {
 		for(y = 0; y < 3; ++y) {
-			if (input == grid[x][y]) {				
+			if (playerMove == grid[x][y]) {				
 				if (arrOutput[x][y] == 'X' || arrOutput[x][y] == 'O') {			// If the position in the grid already taken
 					printf("\nAlready %c - Please Try Again\n", arrOutput[x][y]);
 					return 0;
@@ -84,13 +73,13 @@ int checkInput() {
 	return success;
 }
 
-// Function 4: Output the Grid
-void editGrid() {
+// Output the Grid
+void displayGrid(int playerMove) {
 	int i, j;
-		  
+			  
 	for(i = 0; i < 3; ++i) {
 		for(j = 0; j < 3; ++j) {			
-			if (input == grid[i][j]) {
+			if (playerMove == grid[i][j]) {
 				if (player == 1) arrOutput[i][j] = 'X'; 				// player 1 = X
 				if (player == 2) arrOutput[i][j] = 'O'; 				// player 2 = O
 			}
@@ -102,12 +91,12 @@ void editGrid() {
 	}
 }
 
-// Function 5: Check Winner
+// Check Winner
 int winnerTest() {
-	int i,j,winner = 0;
+	int i, j, winner = 0;
 	
 	for(i = 0; i < 3; ++i) {																								// Check rows
-		for(j=0;j<3;++j) {
+		for(j = 0; j < 3; ++j) {
 			// Check rows, columns, and diagonals for match
 			if (arrOutput[0][0] == arrOutput[0][1] && arrOutput[0][1] == arrOutput[0][2]) winner = arrOutput[0][0]; 	// 123	
 			else if (arrOutput[1][0] == arrOutput[1][1] && arrOutput[1][1] == arrOutput[1][2]) winner = arrOutput[1][0]; 	// 456
@@ -130,3 +119,15 @@ int winnerTest() {
 	
 	return 0;
 }
+
+// Reset the game
+void reset() {
+	char reset[3][3]={'1','2','3','4','5','6','7','8','9'};	
+	int x, y;
+ 
+	for(x = 0; x < 3; ++x)  {
+		for(y = 0; y < 3; ++y) {
+			arrOutput[x][y] = reset[x][y];
+		}
+	}
+}    
